4ed3231caa8a67774ba4b1cbb6a7e4e6dbd173ca
hasiful <hasifulislam32@gmail.com>
2022-10-26 00:48:04 +0600

    home page

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,39 +1,15 @@
-var toggleNav = document.querySelectorAll(".toggle_menu")
-// var subNav = document.querySelectorAll(".drop_nav")
+const accordian = document.getElementsByClassName('accordion_item')
 
-
-toggleNav.forEach(currentItem => {
-     currentItem.addEventListener("click", function () {
-         currentItem.classList.toggle("active_item");
-     })
+;[...accordian].forEach(element => {
+    element.addEventListener('click', function(){
+        ;[...accordian].forEach(child => {
+            child.classList.remove("active-accordion")
+            this.classList.add("active-accordion")
+        });
+    })
 });
 
 
-let loreText = document.getElementById('lorem_text')
-
-loreText.setAttribute("title", "name")
-
-const buttonChange = document.getElementById('buttonChange')
-const popUp = document.getElementById('popup')
-const cross = document.getElementById('cross')
-
-buttonChange.addEventListener("click", function(){
-    popUp.style.cssText = "display: flex"
-})
-
-cross.addEventListener("mouseover", function(){
-    popUp.style.cssText = "display: none"
-})
-
-
-var x = parseFloat('9.6560000000000');
-
-console.log(x.toFixed(10))
-
-
-function numberWithSpaces(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-}
-
-var result = numberWithSpaces(parseFloat(1258485684))
-console.log(result);
+$('.video_popup').magnificPopup({
+    type: 'iframe'
+});
